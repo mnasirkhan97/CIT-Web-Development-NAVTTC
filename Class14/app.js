006e5ca6a7bf2cb87b0dd2310271543073444c48
M Nasir Khan <immnk97@gmail.com>
2024-06-17 16:14:38 +0500

    Class 14

---
+++ Class14/app.js
@@ -0,0 +1,14 @@
+let arrLanguages = ['java', 'python', 'ruby']
+let newValue = arrLanguages[0];
+
+console.log(newValue);
+console.log(arrLanguages)
+
+let newArr = arrLanguages.push('php');
+
+console.log(newArr)
+console.log(arrLanguages)
+
+
+
+console.log(arrLanguages[3]);
