bf35c896a5137e7f5792346dded9045813e02a28
M Nasir Khan <immnk97@gmail.com>
2024-06-18 13:08:03 +0500

    Class 15

---
+++ Class15/app.js
@@ -0,0 +1,24 @@
+// function sum(a, b) { // parameter (Input)
+//     console.log(a + b)
+//     // return a + b; // output
+// }
+
+// sum(5, 10)
+
+// function sum (){
+//     return a + b;
+// }
+
+// var x = 10;
+// var y = 5;
+
+// alert(sum(x,y));
+
+// console.log(sum(x, y));
+
+function myFunc(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunc(2, 4);
+document.getElementById("demo").innerHTML = result;
