46cc641e2fb83e7b53384d2f199dfe2deb94ccdb
M Nasir Khan <immnk97@gmail.com>
2024-06-20 21:47:36 +0500

    Class 16

---
+++ Class16/app.js
@@ -0,0 +1,19 @@
+// function myFun(a, b, c){
+//     a = a + 5;
+//     return a;
+// }
+
+// var a =10;
+// let z = myFun(a, 5, 3);
+// console.log(a)
+// console.log(z)
+
+function myFun(a, b, c) {
+  a = a + 5;
+  return a;
+}
+
+let a = 10;
+let z = myFun(a, 5, 3);
+console.log(`value of a is :` + a);
+console.log(`value of z is :` + z);
