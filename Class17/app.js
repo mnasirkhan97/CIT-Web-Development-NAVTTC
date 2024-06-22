b44dcdfc16ba77fbe0e303fbd0c99998dbba97e7
M Nasir Khan <immnk97@gmail.com>
2024-06-22 17:03:32 +0500

    Class 17

---
+++ Class17/app.js
@@ -0,0 +1,72 @@
+// // Revision
+// var a = 10;
+// function nasir(x,y){
+//     var a = 3;
+//     console.log(a)
+
+//     return x + y;
+// }
+// console.log(a)
+
+// var result = nasir(10, 15)
+
+// console.log(result);
+// console.log(a)
+
+// var str = "Hello Nasir"
+// var str1 = "Good aternoon"
+// console.log(str + " " +  str1);
+// var str1 = str.length
+// console.log(str1)
+//End
+
+// var str = "Hello Nasir"
+// var str1 = "Good aternoon"
+// console.log(str + " " +  str1);
+
+// var user = "My name is Nasir";
+// var user = "My name is Nasir My fav Sports is cricket";
+//replace
+// var user1 = user.replace("My", "Your"); // for singal word
+//replaceAll
+// var user1 = user.replaceAll("My", "Your"); // for multiple words
+//indexOf (1st index 0 se start hoga  )
+// console.log(user.indexOf("n"));
+
+// var username = "User Name Rehman"
+// var userResult = username[2];
+// var userResult = username[7];
+// console.log(userResult)
+
+// console.log(user);
+// console.log(user1)
+
+// let user = "MY TEACHER Name IS ASIM";
+// let user = "my teacher name is sir asim";
+
+// console.log(user.toLowerCase())
+// console.log(user.toUpperCase())
+
+//===> str.substring(starting, ending)
+
+// var str = 'javascript is good language';
+// console.log(str.substring(0,10))
+// console.log(str.substring(3))
+
+//str.trim() : trim start or end ki spaces ko removed krt h
+
+// var str = '           javascript is good language';
+// var str1 = "                         java                 ";
+// console.log(str1);
+// console.log(str1.trim())
+// console.log(str2.trim());
+// console.log(str.trim());
+
+// str.CharAt
+
+var str = "I am Nasir khan                 ";
+console.log(str.substring(5, 10));
+console.log(str.substring(5, str.length));
+console.log(str.length);
+
+// console.log(str.charAt(5));
