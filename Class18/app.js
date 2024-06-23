9f635e81fa5f48d9da2499c5cf3ce111560de36c
M Nasir Khan <immnk97@gmail.com>
2024-06-23 23:03:30 +0500

    Class 18

---
+++ Class18/app.js
@@ -0,0 +1,111 @@
+// =====> If Statement
+// if( 3 < 2 ){
+//     console.log("True")
+// }
+// else{
+//     console.log("False")
+// }
+
+// var a = 7;
+// var b = 5;
+// var c = a - b;
+// if(a > b){
+//     c = 4;
+// }
+// alert(c);
+
+// var a = 7;
+// var b = 5;
+// var c = a - b;
+// if(a < b){
+//     c = 4;
+// }
+// alert(c);
+
+// var a = 7;
+// var b = 5;
+// var c = a - b;
+// if(a > b){
+//     b = 4;
+// }
+// alert(c);
+
+//  var a = 7;
+// var b = 5;
+// var c = a + b;
+// if(c > b){
+//     b = 4;
+//     a = 3;
+// }
+// alert(a);
+
+// var a = 7;
+// var b = 5;
+// var c = a + b;
+// if(c > b){
+//     b = 4;
+//     a = 3;
+//     a = b-c;
+// }
+// alert(a);
+
+// var a = -7;
+// var b = 5;
+// var c = a + b;
+// if(c > b){
+//     b = 4;
+//     a = 3;
+//     a = b-c;
+// }
+// alert(a);
+
+//  =====> Else Statement
+// var x = -5;
+// var y = 3;
+// var z = 0;
+// if(x > y){
+//     z = x;
+// }
+
+// else{
+//     z = y;
+// }
+// alert(z)
+
+//=======> Else If Statement
+
+// var x = -5;
+// var y = 3;
+// var z = 0;
+// if(x < -4){
+//     console.log("if condition")
+// }else if(y < 2 ){
+//     console.log("if else condition")
+// }else{
+//     console.log("else condition")
+// }
+
+// var x = 5;
+// var y = 3;
+// var z = 0;
+// if(x < -5){
+//     console.log("if condition")
+// }else if(y < 2 ){
+//     console.log("if else condition")
+// }else{
+//     console.log("else condition")
+// }
+
+// var x = 5;
+// var y = 3;
+// var z = 0;
+// if(x < -5){
+//     console.log("if condition")
+// }else if(y < 3 ){
+//     console.log("if else condition")
+// }else{
+//     console.log("else condition")
+// }
+
+let num = Math.random();
+console.log(num);
