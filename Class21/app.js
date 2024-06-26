2fec92da7cc6bc20964374242c733df4930cf1d4
M Nasir Khan <immnk97@gmail.com>
2024-06-26 13:18:48 +0500

    Class 21

---
+++ Class21/app.js
@@ -0,0 +1,44 @@
+// function checkPrime() {
+//   var inp = document.getElementById("inputNum").value;
+//   var isPrime = true;
+//   for (var i = 2; i < inp; i++) {
+//     if (inp % i == 0) {
+//       isPrime = false;
+//       break;
+//     } // End of if statement
+//   } // End of for lop
+
+//   if (isPrime) {
+//     alert("The Number is a Prime Number");
+//   } else {
+//     alert("The number is not a Prime Number");
+//   }
+// }
+
+
+// ===> Problem
+
+
+
+// let a = 100;
+// let b = 100;
+// let c = 100;
+
+// if(a==b==c){
+//     console.log("Hello")
+// }else{
+//     console.log("Bye Bye");
+// }
+
+
+
+
+// let a = 100;
+// let b = 100;
+// let c = 100;
+
+// if(a==b==1){
+//     console.log("Hello")
+// }else{
+//     console.log("Bye Bye");
+// }
