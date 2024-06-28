c5b2ddb09f86ea2ebe213534a145963c63d092db
M Nasir Khan <immnk97@gmail.com>
2024-06-28 10:53:45 +0500

    Class 22

---
+++ Class22/app.js
@@ -0,0 +1,66 @@
+// Method ===> 1
+// var i = 1;
+
+// while(i <=10){
+//     document.write(i)
+//     i = i+1;
+// }
+
+// Method ===> 2
+// var i = 1;
+
+// while(i <=10){
+//     document.write(i)
+//     i++;
+
+// }
+
+// Method ===> 3
+// var i = 1;
+
+// while(i <=10){
+//     document.write(i++)
+// }
+//====================================
+// var i = 1;
+
+// while(i <=10){
+//     i++;
+//     document.write(i)                 // 2 se start hoga kiun k print hony se phly i ko increment kr dy ga
+// }
+
+//========================================
+
+// var i = 1;
+
+// while(i <=10){
+//     document.write(i)                 // kbhi khtm na hony wala loop kiun k loop kbi false nh hoga or har waqt chalta rhy ga
+
+// }
+
+// 1 se 19 tk taq adad me values chahiye
+
+//solution:
+//  let i = 1;
+
+// while(i < 20){
+//     document.write(i)
+//     i = i + 2;
+// }
+
+//         let i = 1;
+
+// while(i < 20){
+//     document.write(i)
+//     i = i + 4;
+// }
+
+// Assignment :
+// start for 15, 12, 9, 6, 3
+
+let i = 15;
+
+while (i >= 3) {
+  document.write(i);
+  i = i - 3;
+}
